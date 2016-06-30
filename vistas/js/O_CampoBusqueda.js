66a19b51ab3c329ac8b322bbc13d1fd0d6192e48
vleon-socaportuguesa <vleon@socaportuguesa.com>
2016-06-30 16:15:10 -0400

    Widget campoBusqueda
    este objeto es un campo que permite realizar una consulta a la base de
    datos y montar una lista para seleccionar uno de los elementos mostrados

--- vistas/js/O_CampoBusqueda.js
+++ vistas/js/O_CampoBusqueda.js
@@ -4,10 +4,33 @@ var CampoBusqueda = function(atributos){
     this.construirNodo = function(){
       var nodo = document.createElement('button');
       nodo.classList.add('mat-but-transp');
+      nodo.classList.add('mat-green500');
       nodo.classList.add('icon-search');
       this.nodo = nodo;
-      this.nodo.onclick = function(){
-        console.log('hola');
+      this.nodo.onclick = function activarCampo(){
+        //monto venta modal
+        var ventSelec = UI.crearVentanaModal({
+          contenido: 'ancho'
+        });
+        //agrego la lista a la ventanaModal
+        var lista = UI.agregarLista({
+      		titulo: atributos.titulo,
+          nombre: atributos.nombre,
+          requerido: atributos.requerido,
+      		clase: 'lista',
+      		carga: {
+      			uso:true,
+      			peticion:atributos.peticion,
+      			espera:{
+      				cuadro:atributos.cuadro
+      			},
+            respuesta: atributos.respuesta
+          },
+      		paginacion: {
+      			uso:false
+      		},
+          onclickSlot: asignarSlot
+      	},ventSelec.nodo);
       };
     };
     this.construirNodo();
@@ -16,6 +39,7 @@ var CampoBusqueda = function(atributos){
   this.comboBox = null;
   this.boton = null;
   this.nodo = null;
+  yo = this;
 
   this.construirNodo = function(){
     var nodo = document.createElement('div');
@@ -32,5 +56,28 @@ var CampoBusqueda = function(atributos){
     nodo.appendChild(this.boton.nodo);
     this.nodo = nodo;
   };
+  this.captarValor = function(){
+    return yo.comboBox.captarValor();
+  };
+  this.captarNombre = function(){
+    return yo.comboBox.captarNombre();
+  };
+  this.captarRequerido = function(){
+    return yo.comboBox.requerido;
+  };
+  //funciones internas
+  asignarSlot = function(slot){
+    //agrego opcion al combo
+    var opcion = {
+      nombre:slot.atributos.nombre,
+      codigo:slot.atributos.codigo
+    };
+    yo.comboBox.agregarOpcion(opcion);
+    yo.comboBox.seleccionarOpcion(opcion);
+    //cierro la ventana Modal
+    UI.quitarVentana(yo.atributos.nombre);
+    UI.elementos.modalWindow.eliminarUltimaCapa();
+  };
+
   this.construirNodo();
 };
