effc97aeef6cfb8ad10bea69922635cdae40b4b8
vleon-socaportuguesa <vleon@socaportuguesa.com>
2016-08-09 16:22:06 -0400

    cambios 20160809

--- agronomia/vistas/js/constructores/finca.js
+++ agronomia/vistas/js/constructores/finca.js
@@ -7,47 +7,80 @@ finca = {
 	campos:[
 		{
 		  tipo : 'campoDeTexto',
-		  parametros : {
-		    requerido:true,
-		    titulo:'Codigo Productor',
-		    nombre:'codigo_productor',
-		    tipo:'simple',
-		    eslabon:'simple',
-		    usaToolTip:true
-		  }
+		  parametros : {requerido:true,titulo:'Codigo Productor',nombre:'codigo_productor',tipo:'simple',eslabon:'simple',usaToolTip:true}
 		},
 		{
 		  tipo : 'campoDeTexto',
-		  parametros : {
-		    requerido:true,
-		    titulo:'Letra Finca',
-		    nombre:'letra',
-		    tipo:'simple',
-		    eslabon:'simple',
-		    usaToolTip:true
-		  }
+		  parametros : {requerido:true,titulo:'Letra Finca',nombre:'letra',tipo:'simple',eslabon:'simple',usaToolTip:true}
 		},
 		{
 			tipo: 'campoDeTexto',
-			parametros:{
-				nombre: 'nombre_finca',
-				requerido:true,
-				titulo: 'Nombre',
-				tipo:'simple',
-				eslabon: 'simple',
-				usaToolTip: true
-			}
+			parametros:{nombre: 'nombre_finca',requerido:true,titulo: 'Nombre',tipo:'simple',eslabon: 'simple',usaToolTip: true}
 		},
 		{
 		  tipo : 'campoDeTexto',
-		  parametros : {
-		    requerido:true,
-		    titulo:'Kilimetros al central',
-		    nombre:'titulo',
-		    tipo:'simple',
-		    eslabon:'simple',
-		    usaToolTip:true
-		  }
+		  parametros : {requerido:true,titulo:'Kilimetros al central',nombre:'titulo',tipo:'simple',eslabon:'simple',usaToolTip:true}
+		},{
+	  	tipo : 'campoBusqueda',
+	    parametros : {
+	      titulo:'Tipo de Carretera',
+	      nombre:'codigo_tipo_carretera',
+	      requerido:true,
+				eslabon:'simple',
+	      peticion:{
+		        entidad: 'registroVirtual',
+		        operacion: 'listar',
+						nombre_tabla: 'TIPO_CARRETERA',
+						modulo: 'seguridad'
+		      },
+	      cuadro: {nombre: 'listaTipoCarretera',mensaje: 'Cargando Registros'}
+	    }
+		},
+		{
+	  	tipo : 'campoBusqueda',
+	    parametros : {
+	      titulo:'Tipo de Afiliacion',
+	      nombre:'codigo_tipo_Afiliacion',
+	      requerido:true,
+				eslabon:'simple',
+	      peticion:{
+		        entidad: 'registroVirtual',
+		        operacion: 'listar',
+						nombre_tabla: 'TIPO_AFILIACION',
+						modulo: 'seguridad'
+		      },
+	      cuadro: {nombre: 'listaTipoAfiliacion',mensaje: 'Cargando Registros'}
+	    }
+		},
+		{
+	  	tipo : 'campoBusqueda',
+	    parametros : {
+	      titulo:'Zona',
+	      nombre:'codigo_zona',
+	      requerido:true,
+				eslabon:'simple',
+	      peticion:{
+		        entidad: 'zona',
+		        operacion: 'buscar',
+						modulo: 'agronomia'
+		      },
+	      cuadro: {nombre: 'listaZona',mensaje: 'Cargando Zonas'}
+	    }
+		},
+		{
+	  	tipo : 'campoBusqueda',
+	    parametros : {
+	      titulo:'Municipio',
+	      nombre:'codigo_municipio',
+	      requerido:true,
+				eslabon:'simple',
+	      peticion:{
+		        entidad: 'municipio',
+		        operacion: 'buscar',
+						modulo: 'global'
+		      },
+	      cuadro: {nombre: 'listaTipoAfiliacion',mensaje: 'Cargando Registros'}
+	    }
 		}
 	]
 };
