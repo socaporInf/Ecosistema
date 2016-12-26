2471ac03c640881f8bf5eb57c8caea38ecb17a53
josma-socaportuguesa <josma@socaportuguesa.com>
2016-12-26 08:44:05 -0400

    mejora calendario

--- agronomia/clases/cls_ManejadorCorreo.php
+++ agronomia/clases/cls_ManejadorCorreo.php
@@ -84,7 +84,6 @@ class cls_ManejadorCorreo extends cls_Conexion{
 			if($pos !== false){
 				//extaigo la fecha
 				$subject = explode(' ',$correos[$i]['subject']);
-				print_r($subject);
 				if(($subject[0] == 'RV:')||(trim($subject[0]) == '')){
 					$fecha = $subject[6];
 				}else {
