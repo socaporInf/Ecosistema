9f510b0351a2098edf6d96394afa1188ff0345ad
vrinox <vrinox@htomail.com>
2017-01-03 20:37:58 -0300

    listar faltantes

--- agronomia/clases/cls_Variedad.php
+++ agronomia/clases/cls_Variedad.php
@@ -19,15 +19,16 @@ class cls_Variedad extends cls_Conexion{
 		$lobj_Mensaje = new cls_Mensaje_Sistema;
 		switch ($this->aa_Atributos['operacion']) {
 			case 'buscar':
-				$registros=$this->f_Listar();
-				if(count($registros)!=0){
-					$success=1;
-					$respuesta['registros']=$registros;
-				}else{
-					$respuesta['success'] = 0;
-					$respuesta['mensaje'] = $lobj_Mensaje->buscarMensaje(8);
-				}
-				break;
+				$lb_Enc=$this->f_Listar();
+		       if($lb_Enc){
+		         $success=1;
+		         $respuesta['registros']=$this->aa_Atributos['registros'];
+		         $respuesta['paginas']=$this->aa_Atributos['paginas'];
+		       }else{
+		         $respuesta['success'] = 0;
+		         $respuesta['mensaje'] = $lobj_Mensaje->buscarMensaje(8);
+		       }
+       		break;
 
 			case 'buscarRegistro':
 				$lb_Enc=$this->f_buscar();
@@ -67,8 +68,11 @@ class cls_Variedad extends cls_Conexion{
 	}
 	private function f_Listar(){
 		$x=0;
-		$la_respuesta=array();
-		$ls_Sql="SELECT * FROM agronomia.vvariedad ";
+   		$la_respuesta=array();
+    	$cadenaBusqueda = ($this->aa_Atributos['valor']=='')?'':"where nombre like '%".$this->aa_Atributos['valor']."%'";
+    	$ls_SqlBase="SELECT * FROM agronomia.vvariedad  $cadenaBusqueda";
+    	$orden = " ORDER BY nombre ";
+    	$ls_Sql = $this->f_ArmarPaginacion($ls_SqlBase,$orden);
 		$this->f_Con();
 		$lr_tabla=$this->f_Filtro($ls_Sql);
 		while($la_registros=$this->f_Arreglo($lr_tabla)){
@@ -76,9 +80,11 @@ class cls_Variedad extends cls_Conexion{
 			$la_respuesta[$x]['nombre']=$la_registros['nombre'];
 			$x++;
 		}
-		$this->f_Cierra($lr_tabla);
-		$this->f_Des();
-		return $la_respuesta;
+	   $this->f_Cierra($lr_tabla);
+	   $this->f_Des();
+	   $this->aa_Atributos['registros'] = $la_respuesta;
+	   $lb_Enc=($x == 0)?false:true;
+	   return $lb_Enc;
 	}
 
 	private function f_Buscar(){
